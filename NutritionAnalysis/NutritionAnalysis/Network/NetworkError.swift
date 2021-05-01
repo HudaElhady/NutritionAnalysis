878bfece818cbe086fc31b7eb32573daee3c3901
huda_elhady1 <huda.elhaty@raye7.com>
2021-05-01 15:21:27 +0200

    new project

---
+++ NutritionAnalysis/NutritionAnalysis/Network/NetworkError.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkError.swift
+//  NutritionAnalysis
+//
+//  Created by huda elhady on 01/05/2021.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+    
+    var errorMessage: String {
+        switch self {
+        case .badURL:
+            return "Bad URL"
+        case .requestFailed:
+            return "Internet connection error"
+        case .unknown:
+            return "Unknown error"
+        }
+    }
+    
+}
---
+++ NutritionAnalysis/NutritionAnalysis/Network/ServerManager.swift
@@ -0,0 +1,48 @@
+//
+//  ServerManager.swift
+//  NutritionAnalysis
+//
+//  Created by huda elhady on 01/05/2021.
+//
+
+import Foundation
+
+enum HttpMethod<Body> {
+    case get
+    case post(Body)
+    var method: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        }
+    }
+}
+
+extension HttpMethod {
+    func map<B>(f: (Body)->B) -> HttpMethod<B> {
+        switch self {
+        case .get:
+            return .get
+        case .post (let body):
+            return .post(f(body))
+        }
+    }
+}
+
+extension URLSession {
+    func load<A>(_ resource: Resource<A>, completion: @escaping (Result<A, NetworkError>) -> Void) {
+        dataTask(with: resource.urlRequest) { data, _, error in
+            DispatchQueue.main.async {
+            if data != nil, let result = data.flatMap(resource.parse) {
+                completion(.success(result))
+            } else if error != nil {
+                completion(.failure(.requestFailed))
+            } else {
+                completion(.failure(.unknown))
+            }
+            }
+        }.resume()
+    }
+}
